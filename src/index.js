620e7a505ad80c29621e12fa585f09f5c2f68c64
GiroFrm <girofram844@gmail.com>
2024-10-28 17:33:39 +1100

    feat: Implement dynamic page switching
    - Imported homePage, aboutPage, and contactPage components.
    - Set up initial content to display the homePage.
    - Added event listeners to navigation buttons for switching content:
      - Home button loads homePage.
      - About button loads aboutPage.
      - Contact button loads contactPage.
    - Cleared and updated content container on button clicks.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import "./style.css"
+import homePage from "./homePage";
+import aboutPage from "./aboutPage";
+import contactPage from "./contactPage";
+
+import "./style.css";
+
+const bodyContainer  = document.getElementById('content');
+    bodyContainer.innerHTML = "";
+    bodyContainer.appendChild(homePage())
+
+   const bntHome = document.querySelector('#home-btn');
+         bntHome.addEventListener('click', ()=>{
+            bodyContainer.innerHTML = "";
+            bodyContainer.appendChild(homePage())
+         })
+    const bntAbout = document.querySelector('#about-btn');
+         bntAbout.addEventListener('click', ()=>{
+            bodyContainer.innerHTML = "";
+            bodyContainer.appendChild(aboutPage())
+         })
+    const bntContact = document.querySelector('#contact-btn');
+         bntContact.addEventListener('click', ()=>{
+            bodyContainer.innerHTML = "";
+            bodyContainer.appendChild(contactPage())
+         })
+  
+  
+
+   
